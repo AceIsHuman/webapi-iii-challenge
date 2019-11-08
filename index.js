8b73b733434a163bb46136ffabca600987338f92
AceIsHuman <menoman1995@yahoo.com>
2019-11-07 22:44:32 -0600

    config env port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config();
 const server = require("./server");
+const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
-  console.log("==== Magical things on 5000 =====");
+server.listen(PORT, () => {
+  console.log(`==== Magical things on ${PORT} =====`);
 });
